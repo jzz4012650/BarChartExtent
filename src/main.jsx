28d9942bc523a3eefbcb6f5400e38aaaae007ee7
Jin Zhenzu <jinzhenzu@360.cn>
2016-02-15 17:49:31 +0800

    remove extra code

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ require('./main.less');
 
 import React from 'react';
 import ReactDom from 'react-dom';
-// import Index from './pages/Index.jsx';
 // import AreaChartExtent from './components/AreaChartExtent.jsx';
 import BarChartExtent from './components/BarChartExtent.jsx';
-// import EnhancedInput from './components/EnhancedInput.jsx';
 
 ReactDom.render((
     <BarChartExtent/>
